1cf301cd1ec49b5ec21224a42ad2d62662d9f774
ThunderStormOwl <lausrock@gmail.com>
2021-02-23 10:56:18 -0300

    Completed challenges component created and styled

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/components/Countdown.module.css'
+
+export function Countdown(){
+    return(<div>
+
+    </div>)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 
@@ -12,6 +13,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
         <div>
 
